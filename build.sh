fe4b0d1e0802a2badb555ea51e7debbc6b4e1890
rustdm <dmr.rust@gmail.com>
2019-02-22 14:57:39 -0500

    Added more packages, updated dotfiles

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-PACKAGE_LIST = "xorg-server xorg-xinit i3-gaps i3-status dmenu NetworkManager termite ranger noto-fonts-extra noto-fonts-emoji feh firefox"
+PACKAGE_LIST="xorg-xinit i3-gaps i3status dmenu networkmanager termite compton ranger noto-fonts-extra noto-fonts-emoji feh firefox neofetch"
 
 sudo yes | pacman -Syu
-sudo yes | pacman -S "$PACKAGE_LIST"
+sudo yes "1" | pacman -S xorg-server
+sudo yes | pacman -S $PACKAGE_LIST
 
-wallpaper = "paul-earle-39322-unsplash.jpg"
+WALLPAPER="paul-earle-39322-unsplash.jpg"
 
-echo "feh --bg-scale '$PWD$wallpaper'" > .fehbg 
+echo "feh --bg-scale '$PWD/$WALLPAPER'" > .fehbg 
 
 cp -v .xinitrc /root/
 cp -v .fehbg   /root/
@@ -15,8 +16,9 @@ cp -v .fehbg   /root/
 cd i3
 cp -v config /root/.config/i3/
 
+mkdir /root/.config/termite/
+cp /etc/xdg/termite/config ~/.config/termite/config
+
 cd ..
 cd termite
-cp -v config /root/.config/termite
-
-
+cp -v config /root/.config/termite/
