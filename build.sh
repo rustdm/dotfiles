0073fce7b7d270e4c3a31b1c73376c6f1b536683
rustdm <dmr.rust@gmail.com>
2019-03-03 20:23:25 -0500

    changes to script, adding nano config

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PACKAGE_LIST="xorg-xinit i3-gaps i3status dmenu networkmanager termite compton ranger noto-fonts-extra noto-fonts-emoji feh firefox neofetch"
+PACKAGE_LIST="xorg-server xorg-xinit i3-gaps i3status dmenu networkmanager termite compton ranger noto-fonts-extra noto-fonts-emoji feh firefox neofetch"
 
 sudo yes | pacman -Syu
 sudo yes "1" | pacman -S xorg-server
@@ -12,13 +12,12 @@ echo "feh --bg-scale '$PWD/$WALLPAPER'" > .fehbg
 
 cp -v .xinitrc /root/
 cp -v .fehbg   /root/
+cp -v .nanorc  /root/
 
-cd i3
-cp -v config /root/.config/i3/
+mkdir -p /root/.config/i3/
+cp -v i3/* /root/.config/i3
 
-mkdir /root/.config/termite/
+mkdir -p /root/.config/termite/
 cp /etc/xdg/termite/config ~/.config/termite/config
 
-cd ..
-cd termite
-cp -v config /root/.config/termite/
+cp -v termite/config /root/.config/termite
