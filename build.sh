7a310d51558e3293249f03fd11e04ef8bd06cc46
David Rust <dmr.rust@gmail.com>
2019-11-20 23:07:43 -0500

    simplifying expressions, add non-root user support

--- build.sh
+++ build.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
 
-# sed 's+root+home/david+g' build.sh > buildUser.sh
+# if the currently logged in user is not root replace every occurrance of "root" in the script with "home/user"
+#sed 's+root+home/david+g' build.sh > buildUser.sh
+
+USER=$(whoami)
+
+if [ $(whoami) != 'root' ]
+then
+	BASE="/home/$USER/"
+else
+	BASE="/root/"
+fi
 
 PACKAGE_LIST="xorg-server xorg-xinit i3-gaps i3status dmenu networkmanager termite compton ranger noto-fonts-extra noto-fonts-emoji feh firefox neofetch pulseaudio pavucontrol acpilight"
 PKG_UPDATE="pacman -Syu"
 PKG_INSTALL="pacman -S"
 
-sudo yes | $PKG_UPDATE
-sudo yes "1" | $PKG_INSTALL xorg-server
-sudo yes | $PKG_INSTALL $PACKAGE_LIST
+#sudo yes | $PKG_UPDATE
+#sudo yes "1" | $PKG_INSTALL xorg-server
+#sudo yes | $PKG_INSTALL $PACKAGE_LIST
 
 WALLPAPER="paul-earle-39322-unsplash.jpg"
 
-echo "feh --bg-scale '$PWD/$WALLPAPER'" > .fehbg 
+sudo bash -c "echo \"feh --bg-scale '$PWD/$WALLPAPER'\" > .fehbg"
 
-cp -v .xinitrc /root/
-cp -v .fehbg   /root/
-cp -v .nanorc  /root/
+cp -v .xinitrc .nanorc .fehbg  $BASE
 
-mkdir -p /root/.config/i3/
-cp -v i3/* /root/.config/i3
+mkdir -p $BASE.config/i3/
+cp -v i3/* $BASE.config/i3
 
-mkdir -p /root/.config/termite/
+mkdir -p $BASE.config/termite/
 cp /etc/xdg/termite/config ~/.config/termite/config
 
-cp -v termite/config /root/.config/termite
+cp -v termite/config $BASE.config/termite
